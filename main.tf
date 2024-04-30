2f60300fcc66c2693e4b39891d425087fb23430a
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2024-04-30 21:21:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -188,17 +188,15 @@ resource "aws_instance" "appserver2" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage      = 10
-  db_subnet_group_name   = aws_db_subnet_group.default.id
-  engine                 = "mysql"
-  engine_version         = "8.0.32"
-  instance_class         = "db.t2.micro"
-  multi_az               = false
-  db_name                = "mydb"
-  username               = "raham"
-  password               = "Rahamshaik#444555"
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.database-sg.id]
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "Raham#123568i"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
 }
 
 resource "aws_db_subnet_group" "default" {
