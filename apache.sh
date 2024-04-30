7e7397d160e66428956b0c7f3ed06777c9641fa7
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2024-04-30 21:31:55 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -3,5 +3,5 @@ yum install httpd git -y
 systemctl start httpd
 systemctl status httpd
 cd /var/www/html
-git clone https://github.com/RAHAMSHAIK007/netflixcodde.git
-mv netflixcodde/* .
+git clone https://github.com/RAHAMSHAIK007/netflix-clone.git
+mv netflix-clone/* .
